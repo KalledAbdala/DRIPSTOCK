842fea8dcbf0a23cfe29f82453309bcf8fa1d2bd
Kalled Abdala <kalledgamer@gmail.com>
2026-07-19 17:30:34 -0300

    feat: initial project setup - DRIPSTOCK API

---
+++ dripstock/src/main/java/com/dripstock/DripstockApplication.java
@@ -0,0 +1,13 @@
+package com.dripstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DripstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DripstockApplication.class, args);
+	}
+
+}
---
+++ dripstock/src/test/java/com/dripstock/DripstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dripstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DripstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
